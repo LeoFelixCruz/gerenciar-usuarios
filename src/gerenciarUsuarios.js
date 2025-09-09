c69672884911f179c7932fca4f466ab45f282311
LeoFelixCruz <felixcruz@live.com>
2025-09-09 18:19:44 -0300

    Fazendo a descrição das funções retornarUsuarios e adicionarNovoUsuario

--- src/gerenciarUsuarios.js
+++ src/gerenciarUsuarios.js
@@ -1,10 +1,12 @@
 const usuarios = ['Mateus', 'Marcos', 'Lucas', 'Joao'];
 
 function retornarUsuarios() {
+    // Retorna uma lista de nomes gravados na lista de nomes de usuário.
     return usuarios;
 }
 
 function adicionarNovoUsuario(nome){
+    // Acrescenta um novo nome de usuário passado no momento da chamada da função ao final da lista de usuários.
     usuarios.push(nome);
 }
 
