1a7029fbae01f53546a739bf6c104a941edad0d8
LeoFelixCruz <felixcruz@live.com>
2025-09-09 22:56:05 -0300

    Alterei a lista de nomes de usuários para torna-la em lista de objetos

--- src/gerenciarUsuarios.js
+++ src/gerenciarUsuarios.js
@@ -1,13 +1,26 @@
-const usuarios = ['Mateus', 'Marcos', 'Lucas', 'Joao'];
+const usuarios = [
+    {
+        nome: 'Mateus',
+        email: 'email@test.com'},    
+    {
+        nome: 'Marcos',
+        email: 'email2@test.com'},
+    {      
+        nome: 'Lucas',
+        email: 'email3@test.com'},
+    {
+        nome: 'Joao',
+        email: 'email4@test.com'}
+];
 
 function retornarUsuarios() {
     // Retorna uma lista de nomes gravados na lista de nomes de usuário.
     return usuarios;
 }
 
-function adicionarNovoUsuario(nome){
-    // Acrescenta um novo nome de usuário passado no momento da chamada da função ao final da lista de usuários.
-    usuarios.push(nome);
+function adicionarNovoUsuario(usuario){
+    // Acrescenta um novo usuário passado no momento da chamada da função ao final da lista de usuários.
+    usuarios.push(usuario);
 }
 
 module.exports = {
